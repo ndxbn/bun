53a44f7ba51c4e9282c51687bcc6d8b08f938d20
Sitri Kamishirasawa <wc348nka@gmail.com>
2025-05-11 08:35:30 +0900

    feat: server.ts small example

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log("Here is server.ts");
+import type {Serve} from "bun";
+
+export default {
+	fetch(req) {
+		const responseBody = JSON.stringify([
+			req.method,
+			req.url,
+			req.headers
+		]);
+		const response = new Response(responseBody);
+		response.headers.set("Content-Type", "application/json");
+
+		return response;
+	},
+} satisfies Serve;
