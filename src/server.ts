651f6afcde99cca281006c3b11f3572ed949b685
Sitri Kamishirasawa <wc348nka@gmail.com>
2025-05-11 09:30:27 +0900

    feat: Redis ping

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,29 @@
-import type { Serve } from "bun";
+import {RedisClient} from "bun";
+import type {Serve} from "bun";
+
+// Clients
+const redis = new RedisClient();
 
 export default {
-	fetch(req) {
-		const responseBody = JSON.stringify([req.method, req.url, req.headers]);
-		const response = new Response(responseBody);
+	async fetch(req) {
+		const responseBody = JSON.parse("{}");
+		responseBody.url = req.url;
+		responseBody.Redis = "NG";
+
+		// Redis Connection
+		if (!redis.connected) {
+			await redis.connect();
+		}
+		const pong: "PONG" = await redis.send("PING", []);
+		if (pong === "PONG") {
+			responseBody.Redis = "OK";
+		}
+
+		//
+		const response = new Response(JSON.stringify(responseBody));
 		response.headers.set("Content-Type", "application/json");
 
 		return response;
 	},
+
 } satisfies Serve;
