6af0f920928f71255d1a17df306a0be542f30eff
Sitri Kamishirasawa <wc348nka@gmail.com>
2025-05-12 06:31:39 +0900

    feat: CLI tool scaffold
    - app name を .env から package.json へ移動
    - Console Application の最低限必要そうな実装
    - `bun cli -v` と `bun cli -h` の実装
    - `-V` でログレベルを変更できるようにする予定、という意思表示
    - tsconfig の paths の設定

--- src/cli.ts
+++ src/cli.ts
@@ -1,2 +1,61 @@
 #!/usr/bin/env bun
-console.log("Here is cli.ts");
+import { parseArgs } from "node:util";
+import ConsoleApplication from "@/ConsoleApplication.ts";
+import packageJson from "../package.json" with { type: "json" };
+
+const app = new ConsoleApplication();
+
+const HELP_MESSAGE = `NAME
+	${packageJson.name} - ${packageJson.description}
+OPTIONS:
+	-v, --version  Show version number
+	-h, --help     Show help
+	-V, --verbose  Show verbose output. -VV for more verbose, -VVV for even more verbose.
+`;
+
+const { values } = parseArgs({
+	args: Bun.argv,
+	options: {
+		//
+		version: {
+			type: "boolean",
+			short: "v",
+		},
+		help: {
+			type: "boolean",
+			short: "h",
+		},
+		// output control
+		verbose: {
+			type: "boolean",
+			short: "V",
+			multiple: true,
+		},
+	},
+	strict: false,
+});
+
+// --version と --help は特別な存在なので、最初に処理をする
+// なぜ特別かといえば、
+//   Dockerfile 的に言えば、FROM の直後の TAGS に設定するような、
+//   コンパイル言語であれば #define で宣言するような、
+//   そういう "prepare" 段階で解決できて、Distributed な状態であれば静的に解決できる処理内容なので。
+if (values.version) {
+	// CLI ツールという前提があり、フォーマットを変更したいみたいな需要はないはずなので、
+	//   app.logger ではなく console.log へ直接出力する
+	console.log(packageJson.version);
+	app.exit(0);
+}
+
+if (values.help) {
+	// CLI ツールという前提があり、フォーマットを変更したいみたいな需要はないはずなので、
+	//   app.logger ではなく console.log へ直接出力する
+	console.log(HELP_MESSAGE);
+	app.exit(0);
+}
+
+// 以下、通常の処理
+if (values.verbose) {
+	// todo: app.logger.loglevel で制御するようにする。
+	console.log(values.verbose);
+}
