7125f59ee0da95d9c2d1c2b9d444472f6a7fc792
Sitri Kamishirasawa <wc348nka@gmail.com>
2025-05-12 04:06:49 +0900

    feat: minio service (#143)

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
-import { RedisClient } from "bun";
+import { RedisClient, S3Client } from "bun";
 import type { Serve } from "bun";
 
 // Clients
 const redis = new RedisClient();
+const s3 = new S3Client({
+	// バグで endpoint が http の場合はコンストラクタで直接渡す必要がある
+	// see https://github.com/oven-sh/bun/issues/19011
+	endpoint: "http://localhost:9000",
+});
 
 export default {
 	async fetch(req) {
 		const responseBody = JSON.parse("{}");
 		responseBody.url = req.url;
 		responseBody.Redis = "NG";
+		responseBody.S3 = "NG";
 
 		// Redis Connection
 		if (!redis.connected) {
@@ -17,6 +23,16 @@ export default {
 		const pong: "PONG" = await redis.send("PING", []);
 		if (pong === "PONG") {
 			responseBody.Redis = "OK";
+		}
+
+		// S3 Connection
+		try {
+			await fetch("http://localhost:9000/minio/health/ready");
+			await s3.write("_healthcheck.txt", "");
+			await s3.exists("_healthcheck.txt");
+			responseBody.S3 = "OK";
+		} catch (_) {
+			// noop
 		}
 
 		//
